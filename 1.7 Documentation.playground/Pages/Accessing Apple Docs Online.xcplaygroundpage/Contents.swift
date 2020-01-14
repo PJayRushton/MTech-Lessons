4ff6ae4f9613a41f27949986fd88c4a3affb44c1
Parker Rushton <parker.rushton@octanner.com>
2020-01-14 10:59:33 -0700

    Updates documentation post class

--- 1.7 Documentation.playground/Pages/Accessing Apple Docs Online.xcplaygroundpage/Contents.swift
+++ 1.7 Documentation.playground/Pages/Accessing Apple Docs Online.xcplaygroundpage/Contents.swift
@@ -5,19 +5,37 @@
 //: ## Outside Xcode:
 //: ## Online Docs (developer.apple.com/documentation) (Or just google the class or type you're looking for)
 
-//: **CHALLENGE 1 (Cafe)**: Here's a string:
-let example1 = "Cafe"
-//: This word is actually a french word however, and should have an accent mark on the `e`
+//: **CHALLENGE 1 (WHY?)**: Here's a string:
+let example1 = "Por Que?"
+//: This word is a Spanish word however, and should have an accent mark on the `e`
 
 //: **Look at the documentation for STRING to figure out how to create a string that will print out as the correct spelling with an accent mark**
+
 
 //: **Challenge 2 (PalindrWHAT?)** A Palindrome is a word that is spelled the same backwards and forwards ("racecar", "radar", "kayak", "madam" etc.)
 
 //: Write a function that can determine if a single word is a palindrome (Hint: Keep reading the String Documentation)
 
-//: Another hint: you may need to go beyond apple documentation to get this one to work
+func isPalindromeWord(subject: String) -> Bool {
+    return subject.rever
+}
 
-//: Pro tip-> Dash Mac app
+let shouldBeTrue = isPalindromeWord(subject: "racecar")
+if !shouldBeTrue {
+    print("YOU FAILED")
+}
+let shouldBeFalse = isPalindromeWord(subject: "iOS")
+if shouldBeFalse {
+    print("YOU FAILED!")
+}
+let shouldAlsoBeTrue = isPalindromeWord(subject: "Kayak")
+if !shouldAlsoBeTrue {
+    print("YOU FAILED!")
+}
+
+//: Documentation for `func reversed()`
+//: Documentation for `ReversedCollection<String> `
+//: Documentation for lowercased()
 //: [Next](@next)
 
 
@@ -53,10 +71,10 @@ print(answer)
 
 
 // Challenge 2
-func isPalindromeWord(subject: String) -> Bool {
-    guard !subject.contains(" ") else { return false }
-    return subject.lowercased() == String(subject.lowercased().reversed())
-}
+//func isPalindromeWord(subject: String) -> Bool {
+//    guard !subject.contains(" ") else { return false }
+//    return subject.lowercased() == String(subject.lowercased().reversed())
+//}
 
 let tests = ["racecar", "banana"]
 
